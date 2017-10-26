e0b424d169993c1e93726bb72253f992a1b5f6ce
manishsaraan <manish021js@gmail.com>
2017-10-26 11:11:38 +0530

    socket code added

--- index.js
+++ index.js
@@ -3,117 +3,138 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const httpserver = http.Server(app);
+const httpserver = new http.Server(app);
 const io = require('socket.io')(httpserver);
 
 const fs = require('fs');
 
-const creds = '';
+const redis = require('redis');
+
+let creds = '';
 const port = process.env.PORT || 8080;
 
 // Middleware to setup static folder
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 // Storing users
-const chatters = [];
+let chatters = [];
 
 // Stroing message
-const chat_messages = [];
+let chat_messages = [];
 
+// Read the credential from cred.json file
+fs.readFile('creds.json', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  creds = JSON.parse(data);
+  const client = redis.createClient();
 
-//Read the credential from cred.json file
-fs.readFile('creds.json',function(err, data){
-    if(err) throw err;
-    creds = JSON.parse(data);
-    client = redis.createClient();
-
-    //on client ready
-    client.once('ready',function(){
-    	//get users
-    	client.get('chat_users',function(err, reply){
-    		if(reply){
-    			chatters = JSON.parse(reply);
-    		}
-    	});
-
-    	//getting messages
-    	client.get('chat_app_messages',function(err, reply){
-    		if(reply){
-    			chat_messages = JSON.parse(reply);
-    		}
-    	});
+    // On client ready
+  client.once('ready', () => {
+        // Get users
+    client.get('chat_users', (err, reply) => {
+      if (err) {
+        throw err;
+      }
+      if (reply) {
+        chatters = JSON.parse(reply);
+      }
     });
 
+        // Getting messages
+    client.get('chat_app_messages', (err, reply) => {
+      if (err) {
+        throw err;
+      }
+      if (reply) {
+        chat_messages = JSON.parse(reply);
+      }
+    });
+  });
 });
 
 /**
  * Routes Starts here
  */
 
-//join chat api
-app.post('/join',function(req, res){
-    var username = req.body.username;
+// join chat api
+app.post('/join', (req, res) => {
+  const username = req.body.username;
 
-    //validate username
-    if(chatters.indexOf(username) === -1){
-    	chatters.push(username);
+    // Validate username
+  if (chatters.indexOf(username) === -1) {
+    chatters.push(username);
 
-    	//save in redis
-    	client.set('chat_users',JSON.stringify(chatters));
-    	res.send({
-    		 'chatters' : chatters,
-    		 'status'  : 'OK'
-    	});
-    }
-    else
-    {
-        res.send({    		 
-    		 'status'  : 'FAILED'
-    	})
-    }
-});
-
-//leave room api
-app.post('/leave',function(err, res){
-	var username = req.body.username;
-	chatters.splice(chatters.indexOf(username), 1);
-
-	//update redis
-	client.set('chat_users',JSON.stringify(chatters));
-	res.send({
-        'status': 'OK'
+        // Save in redis
+    client.set('chat_users', JSON.stringify(chatters));
+    res.send({
+      chatters,
+      status: 'OK'
     });
-});
-
-//storing the messages
-app.post('/send_message',function(req, res){
-	var username = req.body.username;
-	var message = req.body.message;
-	chat_messages.push({
-		message :  message,
-		sender  :  username
-	});
-
-	//updating redis
-	client.set('chat_app_messages',JSON.stringify(chat_messages));
-	res.send({
-        'status': 'OK'
+  } else {
+    res.send({
+      status: 'FAILED'
     });
+  }
 });
 
-//get all messages
-app.get('/get_messages',function(req, res){
-	res.send(chat_messages);
+// Leave room api
+app.post('/leave', (err, res) => {
+  const username = req.body.username;
+  chatters.splice(chatters.indexOf(username), 1);
+
+	// Update redis
+  client.set('chat_users', JSON.stringify(chatters));
+  res.send({
+    status: 'OK'
+  });
+});
+
+// Storing the messages
+app.post('/send_message', (req, res) => {
+  const username = req.body.username;
+  const message = req.body.message;
+  chat_messages.push({
+    message,
+    sender: username
+  });
+
+	// Updating redis
+  client.set('chat_app_messages', JSON.stringify(chat_messages));
+  res.send({
+    status: 'OK'
+  });
+});
+
+// Get all messages
+app.get('/get_messages', (req, res) => {
+  res.send(chat_messages);
 });
 
 /**
- * Routes ends here
+ * Socket Starts here
  */
+
+io.on('connection', function(socket) {
+    //send message
+    socket.on('message', function(data) {
+        io.emit('send', data);
+    });
+    
+    //update users
+    socket.on('update_chatter_count', function(data) {
+        io.emit('count_chatters', data);
+    });
+});
+
+
+
 
 // Start server
 httpserver.listen(port, () => {
-	console.log('Server is running on port : ' + port);
+  console.log('Server is running on port : ' + port);
 });
